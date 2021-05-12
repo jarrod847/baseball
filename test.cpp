59b463d3aa3bc7a99fb98e4e0d0d3fad5b2ef1bf
jarrodskahill <jarrodskahill@gmail.com>
2021-05-11 20:15:59 -0500

    messing around with c++"

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int foo[3] = {3, 2, 9};
+    int a, b;
+    int result;
+    a = 5;
+    b = 2;
+    a = a + 1;
+    foo[0] += 3;
+    cout << foo[0];
+
+    return 0;
+}
